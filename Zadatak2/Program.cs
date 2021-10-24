81e489d04e181bea2b9ca51eb9a5a1ce55a77856
Vidan Lugonja <vidan.lugonja@gmail.com>
2021-10-24 20:52:38 +0200

    Add files via upload

---
+++ Zadatak2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Zadatak2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // broj A
+            Console.WriteLine("UNESI BREDNOST A:");
+            int a = Int32.Parse(Console.ReadLine());
+
+            // broj B
+            Console.WriteLine("UNESI BREDNOST B:");
+            int b = Int32.Parse(Console.ReadLine());
+
+            // broj C
+            Console.WriteLine("UNESI BREDNOST C:");
+            int c = Int32.Parse(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+                case 3:
+                    Console.WriteLine("Vrednost A je 1 ili 3");
+                    break;
+                case 2:
+                    b = c--;
+                    Console.WriteLine($"{b} {c}");
+                    break;
+                default:
+                    b = c == 4 ? 5 : 6;
+                    break;
+            }
+            Console.WriteLine($"A = {a} / B = {b} / C = {c}");
+        }
+    }
+}
